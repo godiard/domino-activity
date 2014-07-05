1e0cb29154aa616769753b01848a4afd32d0d730
Gonzalo Odiard <godiard@gmail.com>
2014-07-05 02:05:37 -0300

    Pep8 & pyflakes fixes

--- cairoutils.py
+++ cairoutils.py
@@ -1,25 +1,28 @@
-from gi.repository import GObject
-from gi.repository import Gtk
-import cairo
+def draw_round_rect(context, x, y, w, h, r):
+    # Copiado de http://www.steveanddebs.org/PyCairoDemo/
+    # "Draw a rounded rectangle"
+    #   A****BQ
+    #  H      C
+    #  *      *
+    #  G      D
+    #   F****E
 
-
-def draw_round_rect(context,x,y,w,h,r):
-	# Copiado de http://www.steveanddebs.org/PyCairoDemo/
-	# "Draw a rounded rectangle"
-	#   A****BQ
-	#  H      C
-	#  *      *
-	#  G      D
-	#   F****E
-
-	context.move_to(x+r,y)                      # Move to A
-	context.line_to(x+w-r,y)                    # Straight line to B
-	context.curve_to(x+w,y,x+w,y,x+w,y+r)       # Curve to C, Control points are both at Q
-	context.line_to(x+w,y+h-r)                  # Move to D
-	context.curve_to(x+w,y+h,x+w,y+h,x+w-r,y+h) # Curve to E
-	context.line_to(x+r,y+h)                    # Line to F
-	context.curve_to(x,y+h,x,y+h,x,y+h-r)       # Curve to G
-	context.line_to(x,y+r)                      # Line to H
-	context.curve_to(x,y,x,y,x+r,y)             # Curve to A
-	return
-
+    # Move to A
+    context.move_to(x + r, y)
+    # Straight line to B
+    context.line_to(x + w - r, y)
+    # Curve to C, Control points are both at Q
+    context.curve_to(x + w, y, x + w, y, x + w, y + r)
+    # Move to D
+    context.line_to(x + w, y + h - r)
+    # Curve to E
+    context.curve_to(x + w, y + h, x + w, y + h, x + w - r, y + h)
+    # Line to F
+    context.line_to(x + r, y + h)
+    # Curve to G
+    context.curve_to(x, y + h, x, y + h, x, y + h - r)
+    # Line to H
+    context.line_to(x, y + r)
+    # Curve to A
+    context.curve_to(x, y, x, y, x + r, y)
+    return
--- dominopiece.py
+++ dominopiece.py
@@ -1,196 +1,158 @@
 #!/usr/bin/env python
 
-### Piezas del domino
-### By Gonzalo Odiard, 2006 godiard at gmail.com
-### GPL License - http://www.gnu.org/copyleft/gpl.html
+# Piezas del domino
+# By Gonzalo Odiard, 2006 godiard at gmail.com
+# GPL License - http://www.gnu.org/copyleft/gpl.html
 
-
-from gi.repository import GObject
-from gi.repository import Gtk
-import cairo
-import math, random
-
-import gi
-gi.require_version('Gtk', '3.0')
-import getopt
-
-import logging
-from gettext import gettext as _
-import sugar3
-from sugar3.graphics import xocolor
 from sugar3.graphics import style
 import dominoview
 import cairoutils
 
-import dominopieceprocessor
 
 class DominoPiece:
-	""" Informacion de cada pieza del juego (visible o no) """
+    """ Informacion de cada pieza del juego (visible o no) """
 
-	PIECE_PLAYER = 1
-	PIECE_IN_MOVE = 2
-	PIECE_PLACED = 3
-	PIECE_WAITING = 0
+    PIECE_PLAYER = 1
+    PIECE_IN_MOVE = 2
+    PIECE_PLACED = 3
+    PIECE_WAITING = 0
 
-	def __init__(self,a,b):
-		#print "creando pieza",n,p
-		
-		self.a = a
-		self.b = b
-		self.textA = str(a)
-		self.textB = str(b)
-		self.vertical = False
-		self.reversed = False
-		self.player = None
-		self.state = DominoPiece.PIECE_WAITING
-		self.visible = False
-		self._itemA = None
-		self._itemB = None
+    def __init__(self, a, b):
+        # print "creando pieza",n,p
 
-#	def selected(self):
-#		if (self.player == None):
-#			return False
-#		if (self.player.order_piece_selected == -1):
-#			return False
-#		if (self.player.order_piece_selected >= len(self.player.get_pieces())):
-#			return False
-#		if (self.player.get_pieces()[self.player.order_piece_selected] == self):
-#			return True
+        self.a = a
+        self.b = b
+        self.textA = str(a)
+        self.textB = str(b)
+        self.vertical = False
+        self.reversed = False
+        self.player = None
+        self.state = DominoPiece.PIECE_WAITING
+        self.visible = False
+        self._itemA = None
+        self._itemB = None
 
+    def draw(self, ctx, selected):
+        SIZE = dominoview.SIZE
+        # if self.vertical:
+        #   width = SIZE + SIZE/8 + 2
+        #   height = SIZE*2 + SIZE/8 + 2
+        # else:
+        #   width = SIZE*2 + SIZE/8 + 2
+        #   height = SIZE + SIZE/8 + 2
 
-	def draw(self,ctx,selected):
-		SIZE = dominoview.SIZE
-		#if self.vertical:
-		#	width = SIZE + SIZE/8 + 2
-		#	height = SIZE*2 + SIZE/8 + 2
-		#else:
-		#	width = SIZE*2 + SIZE/8 + 2
-		#	height = SIZE + SIZE/8 + 2
+        ctx.save()
+        ctx.translate(self.x, self.y)
+        ctx.move_to(self.x, self.y)
+        # ctx.translate(1,1)
+        # ctx.move_to(1, 1)
+        r = 10
+        stroke_r, stroke_g, stroke_b = 0, 0, 0
+        fill_r, fill_g, fill_b = 1, 1, 1
+        alpha = 1
+        if self.player.color is not None:
+            xocolor = self.player.color
+            stroke_r, stroke_g, stroke_b, alpha = style.Color(
+                xocolor.get_stroke_color(), 1.0).get_rgba()
+            fill_r, fill_g, fill_b, alpha = style.Color(
+                xocolor.get_fill_color(), 1.0).get_rgba()
 
-		ctx.save()
-		ctx.translate(self.x,self.y)
-		ctx.move_to(self.x,self.y)
-		#ctx.translate(1,1)
-		#ctx.move_to(1, 1)
-		r = 10 
-		stroke_r,stroke_g,stroke_b = 0,0,0
-		fill_r,fill_g,fill_b = 1, 1, 1
-		alpha = 1
-		if (self.player.color != None):
-			xocolor = self.player.color
-			stroke_r,stroke_g,stroke_b,alpha = style.Color(xocolor.get_stroke_color(),1.0).get_rgba()
-			fill_r,fill_g,fill_b,alpha = style.Color(xocolor.get_fill_color(),1.0).get_rgba()
-			
+        if self.vertical:
+            cairoutils.draw_round_rect(ctx, -1, -1, SIZE + 2, SIZE * 2 + 2, r)
 
-		if self.vertical:
-			cairoutils.draw_round_rect(ctx,-1,-1,SIZE+2,SIZE*2+2,r)
-			
-			ctx.set_source_rgb (stroke_r,stroke_g,stroke_b)
-			ctx.fill()
-			
-			cairoutils.draw_round_rect(ctx,0,0,SIZE,SIZE*2,r)
-			if selected:
-				ctx.set_source_rgb (1, 1, 204.0/255.0)
-			else: 
-				ctx.set_source_rgb (fill_r,fill_g,fill_b)
-			ctx.fill()
+            ctx.set_source_rgb(stroke_r, stroke_g, stroke_b)
+            ctx.fill()
 
-			ctx.move_to(SIZE,0)
-			ctx.rel_line_to((SIZE/8),(SIZE/8))
-			ctx.rel_line_to(0,SIZE*2)
-			ctx.rel_line_to(-(SIZE/8),-(SIZE/8))
-			ctx.rel_line_to(0,-SIZE*2)
-			ctx.close_path()
-			ctx.fill_preserve()
-			ctx.set_source_rgb (stroke_r,stroke_g,stroke_b)
-			ctx.stroke()
+            cairoutils.draw_round_rect(ctx, 0, 0, SIZE, SIZE * 2, r)
+            if selected:
+                ctx.set_source_rgb(1, 1, 204.0 / 255.0)
+            else:
+                ctx.set_source_rgb(fill_r, fill_g, fill_b)
+            ctx.fill()
 
-			ctx.move_to(0,SIZE*2)
-			ctx.rel_line_to((SIZE/8),(SIZE/8))
-			ctx.rel_line_to(SIZE,0)
-			ctx.rel_line_to(-(SIZE/8),-(SIZE/8))
-			ctx.rel_line_to(-SIZE,0)
-			ctx.close_path()
-			ctx.set_source_rgb (fill_r,fill_g,fill_b)
-			ctx.fill_preserve()
-			ctx.set_source_rgb (stroke_r,stroke_g,stroke_b)
-			ctx.stroke()
+            ctx.move_to(SIZE, 0)
+            ctx.rel_line_to((SIZE / 8), (SIZE / 8))
+            ctx.rel_line_to(0, SIZE * 2)
+            ctx.rel_line_to(-(SIZE / 8), -(SIZE / 8))
+            ctx.rel_line_to(0, -SIZE * 2)
+            ctx.close_path()
+            ctx.fill_preserve()
+            ctx.set_source_rgb(stroke_r, stroke_g, stroke_b)
+            ctx.stroke()
 
-			#ctx.move_to(SIZE/4,SIZE)
-			#ctx.rel_line_to(SIZE/2,0)
-			#ctx.stroke()
+            ctx.move_to(0, SIZE * 2)
+            ctx.rel_line_to((SIZE / 8), (SIZE / 8))
+            ctx.rel_line_to(SIZE, 0)
+            ctx.rel_line_to(- (SIZE / 8), -(SIZE / 8))
+            ctx.rel_line_to(-SIZE, 0)
+            ctx.close_path()
+            ctx.set_source_rgb(fill_r, fill_g, fill_b)
+            ctx.fill_preserve()
+            ctx.set_source_rgb(stroke_r, stroke_g, stroke_b)
+            ctx.stroke()
 
+            if not self.reversed:
+                self._draw_label_a(ctx, 0, 0)
+                self._draw_label_b(ctx, 0, SIZE)
+            else:
+                self._draw_label_b(ctx, 0, 0)
+                self._draw_label_a(ctx, 0, SIZE)
 
-			if not self.reversed:
-				self._draw_label_a(ctx,0,0)
-				self._draw_label_b(ctx,0,SIZE)
-			else:
-				self._draw_label_b(ctx,0,0)
-				self._draw_label_a(ctx,0,SIZE)
+        else:
+            cairoutils.draw_round_rect(ctx, -1, -1, SIZE * 2 + 2, SIZE + 2, r)
+            ctx.set_source_rgb(stroke_r, stroke_g, stroke_b)
+            ctx.fill()
 
-		else:
-			cairoutils.draw_round_rect(ctx,-1,-1,SIZE*2+2,SIZE+2,r)
-			ctx.set_source_rgb (stroke_r,stroke_g,stroke_b)
-			ctx.fill()
+            cairoutils.draw_round_rect(ctx, 0, 0, SIZE * 2, SIZE, r)
+            if selected:
+                ctx.set_source_rgb(1, 1, 204.0 / 255.0)
+            else:
+                ctx.set_source_rgb(fill_r, fill_g, fill_b)
+            ctx.fill()
 
-			cairoutils.draw_round_rect(ctx,0,0,SIZE*2,SIZE,r)
-			if selected:
-				ctx.set_source_rgb (1, 1, 204.0/255.0)
-			else: 
-				ctx.set_source_rgb (fill_r,fill_g,fill_b)
-			ctx.fill()
+            ctx.move_to(SIZE * 2, 0)
+            ctx.rel_line_to((SIZE / 8), (SIZE / 8))
+            ctx.rel_line_to(0, SIZE)
+            ctx.rel_line_to(-(SIZE / 8), -(SIZE / 8))
+            ctx.rel_line_to(0, -SIZE)
+            ctx.close_path()
+            ctx.fill_preserve()
+            ctx.set_source_rgb(stroke_r, stroke_g, stroke_b)
+            ctx.stroke()
 
-			ctx.move_to(SIZE*2,0)
-			ctx.rel_line_to((SIZE/8),(SIZE/8))
-			ctx.rel_line_to(0,SIZE)
-			ctx.rel_line_to(-(SIZE/8),-(SIZE/8))
-			ctx.rel_line_to(0,-SIZE)
-			ctx.close_path()
-			ctx.fill_preserve()
-			ctx.set_source_rgb (stroke_r,stroke_g,stroke_b)
-			ctx.stroke()
+            ctx.move_to(0, SIZE)
+            ctx.rel_line_to((SIZE / 8), (SIZE / 8))
+            ctx.rel_line_to(SIZE * 2, 0)
+            ctx.rel_line_to(-(SIZE / 8), -(SIZE / 8))
+            ctx.rel_line_to(-SIZE * 2, 0)
+            ctx.close_path()
+            ctx.set_source_rgb(fill_r, fill_g, fill_b)
+            ctx.fill_preserve()
+            ctx.set_source_rgb(stroke_r, stroke_g, stroke_b)
+            ctx.stroke()
 
-			ctx.move_to(0,SIZE)
-			ctx.rel_line_to((SIZE/8),(SIZE/8))
-			ctx.rel_line_to(SIZE*2,0)
-			ctx.rel_line_to(-(SIZE/8),-(SIZE/8))
-			ctx.rel_line_to(-SIZE*2,0)
-			ctx.close_path()
-			ctx.set_source_rgb (fill_r,fill_g,fill_b)
-			ctx.fill_preserve()
-			ctx.set_source_rgb (stroke_r,stroke_g,stroke_b)
-			ctx.stroke()
+            if not self.reversed:
+                self._draw_label_a(ctx, 0, 0)
+                self._draw_label_b(ctx, SIZE, 0)
+            else:
+                self._draw_label_b(ctx, 0, 0)
+                self._draw_label_a(ctx, SIZE, 0)
 
-			#ctx.move_to(SIZE/4,0)
-			#ctx.rel_line_to(0,SIZE-SIZE/4)
-			#ctx.stroke()
+        ctx.restore()
 
-			if not self.reversed:
-				self._draw_label_a(ctx,0,0)
-				self._draw_label_b(ctx,SIZE,0)
-			else:
-				self._draw_label_b(ctx,0,0)
-				self._draw_label_a(ctx,SIZE,0)
+    def _draw_label_a(self, ctx, x, y):
+        self.player.game.processor.draw_label(ctx, self, self.textA, x, y)
 
+    def _draw_label_b(self, ctx, x, y):
+        self.player.game.processor.draw_label(ctx, self, self.textB, x, y)
 
-		ctx.restore()	
-
-	def _draw_label_a(self,ctx,x,y):
-		self.player.game.processor.draw_label(ctx,self,self.textA,x,y)
-		
-	def _draw_label_b(self,ctx,x,y):
-		self.player.game.processor.draw_label(ctx,self,self.textB,x,y)
-
-
-	def rotate(self):
-		vertical_ini = self.vertical
-		reversed_ini = self.reversed		
-		if self.vertical: 
-			self.reversed = not self.reversed					
-		self.vertical = not self.vertical 
-		n,p = self.player.game.table.get_tile_coord(self.x,self.y)
-		if (not self.player.game.test_in_board(self,n,p)):
-			self.vertical = vertical_ini
-			self.reversed = reversed_ini 
-
-
+    def rotate(self):
+        vertical_ini = self.vertical
+        reversed_ini = self.reversed
+        if self.vertical:
+            self.reversed = not self.reversed
+        self.vertical = not self.vertical
+        n, p = self.player.game.table.get_tile_coord(self.x, self.y)
+        if not self.player.game.test_in_board(self, n, p):
+            self.vertical = vertical_ini
+            self.reversed = reversed_ini
